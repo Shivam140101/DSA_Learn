7c76cbcfb1538cac4dc9c1d61afd79c1e67a52e2
Shivam <shivamahlawat118@gmail.com>
2024-05-01 23:25:35 +0530

    mesage

---
+++ src/Strings/Anagram.java
@@ -0,0 +1,36 @@
+package Strings;
+
+import java.util.HashMap;
+
+public class Anagram {
+
+
+    public static boolean isAnagram(String str1, String str2) {
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        if (str1.length() != str2.length()) return false;
+
+        for(char c : str1.toCharArray()){
+            map.put(c , map.getOrDefault(c,0)+1);
+        }
+
+        for(char c : str2.toCharArray()){
+            if (!map.containsKey(c))
+                return false;
+            map.put(c,map.get(c)-1);
+            if (map.get(c) < 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String str1 = "bbaa";
+        String str2 = "baa";
+
+        boolean anagram = isAnagram(str1, str2);
+        System.out.println(anagram);
+    }
+}

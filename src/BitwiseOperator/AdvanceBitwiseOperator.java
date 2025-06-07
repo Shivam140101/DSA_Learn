131e6b31a8090898e6b360f89b771448f08e1c02
Shivam <shivamahlawat118@gmail.com>
2025-06-07 14:34:56 +0530

    message

---
+++ src/BitwiseOperator/AdvanceBitwiseOperator.java
@@ -0,0 +1,37 @@
+package BitwiseOperator;
+
+public class AdvanceBitwiseOperator {
+    public static void main(String[] args) {
+
+        /* ********** Bitwise NOT(~) ************* */
+
+//        int x = 5;
+//        System.out.println(~x);
+
+        /* ********** Left Shift(<<) ************* */
+
+//        int x = 3;
+//        System.out.println(x << 1);
+//        System.out.println(x << 2);
+//        int y = 4;
+//        System.out.println(x << y);
+
+        /* ********** Signed Right Shift(>>) ************* */
+
+//        int x = -33;
+//        System.out.println(x >> 1);
+//        System.out.println(x >> 2);
+//        int y = 4;
+//        System.out.println(x >> y);
+
+        /* ********** Unsigned Right Shift(>>>) ************* */
+
+        int x = 33;
+        System.out.println(x >>> 1);
+        System.out.println(x >>> 2);
+        int y = 4;
+        System.out.println(x >>> y);
+
+
+    }
+}
---
+++ src/BitwiseOperator/BasicBitwiseOperator.java
@@ -0,0 +1,33 @@
+package BitwiseOperator;
+
+public class BasicBitwiseOperator {
+    public static void main(String[] args) {
+
+
+        /* ********** Bitwise AND(&) ************* */
+
+//        int x = 3;
+//        int y = 6;
+//        int z = x & y;
+//
+//        System.out.println(z);
+
+        /* ********** Bitwise OR(|) ************* */
+
+//        int x = 3;
+//        int y = 6;
+//        int z = x | y;
+//
+//        System.out.println(z);
+
+        /* ********** Bitwise XOR(^) ************* */
+
+        int x = 3;
+        int y = 6;
+        int z = x ^ y;
+
+        System.out.println(z);
+
+
+    }
+}
